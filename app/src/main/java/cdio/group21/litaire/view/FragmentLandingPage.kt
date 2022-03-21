0eb3b8d2061487da052309bdee5c13c5d4878d8c
Lucas Skals <skalslucas@gmail.com>
2022-03-21 19:19:15 +0100

    java: Added permissions for camera to manifest

--- app/src/main/java/cdio/group21/litaire/view/FragmentLandingPage.kt
+++ app/src/main/java/cdio/group21/litaire/view/FragmentLandingPage.kt
@@ -1,6 +1,10 @@
 package cdio.group21.litaire.view
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
