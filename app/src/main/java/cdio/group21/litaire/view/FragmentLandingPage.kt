da2174d2b70e063a1f8774c84cbe0b87ef973da3
Lucas Skals <skalslucas@gmail.com>
2022-03-21 20:11:07 +0100

    FragmentLandingPage: Added functions to take picture.

--- app/src/main/java/cdio/group21/litaire/view/FragmentLandingPage.kt
+++ app/src/main/java/cdio/group21/litaire/view/FragmentLandingPage.kt
@@ -3,14 +3,21 @@ package cdio.group21.litaire.view
 import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.navigation.fragment.findNavController
 import cdio.group21.litaire.databinding.FragmentLandingPageBinding
+import com.swein.easypermissionmanager.EasyPermissionManager
+import java.io.File
+import java.util.jar.Manifest
 
 
 class FragmentLandingPage : Fragment() {
@@ -18,6 +25,21 @@ class FragmentLandingPage : Fragment() {
     private var _binding: FragmentLandingPageBinding? = null
 
     private val binding get() = _binding!!
+
+
+
+    private val selectPictureLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){
+        binding.ivInfo1.setImageURI(it)
+    }
+
+    private var tempImageUri: Uri? =null
+    private var tempImageFilePath = ""
+    private val cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()){ success ->
+        if (success){
+            binding.ivInfo1.setImageURI(tempImageUri)
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,10 +51,35 @@ class FragmentLandingPage : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val easyPermissionManager = EasyPermissionManager(this.requireActivity())
+        easyPermissionManager.requestPermission(
+            "permission",
+            "permissions are necessary",
+            "setting",
+            arrayOf(
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            )
+
+        binding.ivCameraButton.setOnClickListener() {
+            tempImageUri = FileProvider.getUriForFile(this.requireContext(),
+                "cdio.group21.litaire.provider", createImageFile().also {
+                    tempImageFilePath = it.absolutePath
+                })
+            cameraLauncher.launch(tempImageUri)
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun createImageFile() : File{
+        val storageDirectory = this.activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("temp_image", "jpg", storageDirectory)
     }
 }
