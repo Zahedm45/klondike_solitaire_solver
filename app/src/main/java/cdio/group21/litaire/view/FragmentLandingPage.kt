3ba7efc7a1917ac5cc7695dc4d6aae52e6662599
Lucas Skals <skalslucas@gmail.com>
2022-03-22 07:48:20 +0100

    FragmentLandingPage: Deleted unused import statements

--- app/src/main/java/cdio/group21/litaire/view/FragmentLandingPage.kt
+++ app/src/main/java/cdio/group21/litaire/view/FragmentLandingPage.kt
@@ -1,23 +1,18 @@
 package cdio.group21.litaire.view
 
-import android.app.Activity
-import android.content.ActivityNotFoundException
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
-import androidx.navigation.fragment.findNavController
 import cdio.group21.litaire.databinding.FragmentLandingPageBinding
 import com.swein.easypermissionmanager.EasyPermissionManager
 import java.io.File
-import java.util.jar.Manifest
+
 
 
 class FragmentLandingPage : Fragment() {
@@ -44,7 +39,7 @@ class FragmentLandingPage : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentLandingPageBinding.inflate(inflater, container, false)
         return binding.root
     }
