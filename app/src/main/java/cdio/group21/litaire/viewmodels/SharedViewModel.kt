efbf18d67b738bcfcfe4243ff1f940a815f9f3f6
Lucas Skals <skalslucas@gmail.com>
2022-03-22 19:29:17 +0100

    view: Added set/get functions in SharedViewModel.kt

--- app/src/main/java/cdio/group21/litaire/viewmodels/SharedViewModel.kt
+++ app/src/main/java/cdio/group21/litaire/viewmodels/SharedViewModel.kt
@@ -1,18 +1,22 @@
 package cdio.group21.litaire.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import cdio.group21.litaire.data.Card
+import java.net.URI
 
 class SharedViewModel : ViewModel() {
     private val cardNumber = MutableLiveData<Int>()
     private val cardType = MutableLiveData<Enum<Card>>()
+    private val imageURI = MutableLiveData<URI>()
 
-    private fun cardNumber(number : Int){
-        cardNumber.value = number
+    fun setImageURI(uri: URI){
+        imageURI.value = uri
     }
 
-    private fun cardType(cardType : Enum<Card>){
+    fun getImageURI() : LiveData<URI>{
+        return imageURI
     }
 
 
