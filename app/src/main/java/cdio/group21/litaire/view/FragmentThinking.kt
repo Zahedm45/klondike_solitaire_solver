451b42ecb8100ccd2ac5d105b7201557ad02c320
Lucas Skals <skalslucas@gmail.com>
2022-03-22 18:29:37 +0100

    view: Added fragment_thinking.xml

--- app/src/main/java/cdio/group21/litaire/view/FragmentThinking.kt
+++ app/src/main/java/cdio/group21/litaire/view/FragmentThinking.kt
@@ -1,31 +1,26 @@
 package cdio.group21.litaire.view
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import cdio.group21.litaire.R
 
 class FragmentThinking : Fragment() {
-
-    companion object {
-        fun newInstance() = FragmentThinking()
-    }
-
-    private lateinit var viewModel: FragmentThinkingVM
+    //Shared viewmodel between fragments because some data is shared.
+    private val viewModel: FragmentThinkingVM by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_thinking_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_thinking, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FragmentThinkingVM::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         // TODO: Use the ViewModel
     }
 
