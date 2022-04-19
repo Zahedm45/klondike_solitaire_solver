f003632f1fee5886b99ba37587ca5e5f3159e37a
Zahed <zahedm45@gmail.com>
2022-04-19 12:18:58 +0200

    ML: Added a new model.

--- app/src/main/java/cdio/group21/litaire/tflite/ObjectRecognition.kt
+++ app/src/main/java/cdio/group21/litaire/tflite/ObjectRecognition.kt
@@ -26,7 +26,7 @@ import org.tensorflow.lite.task.vision.detector.ObjectDetector
                 .build()
             val detector = ObjectDetector.createFromFileAndOptions(
                 context,
-                "spade8_2.tflite",
+                "epochs3_playing_cards.tflite",
                 options
             )
 
