b78c723b3e1070f82d57c4548a8380c76c40aadc
Zahed <zahedm45@gmail.com>
2022-06-03 11:55:56 +0200

    ML: A new class has been added

---
+++ app/src/main/java/cdio/group21/litaire/viewmodels/solver/Solver.kt
@@ -0,0 +1,15 @@
+package cdio.group21.litaire.viewmodels.solver
+
+import cdio.group21.litaire.data.DetectionResult
+import cdio.group21.litaire.data.SortedResult
+
+class Solver {
+
+    var waste = null
+    var foundation: ArrayList<DetectionResult> = ArrayList()
+    var tableaus: ArrayList<SortedResult> = ArrayList()
+
+
+
+
+}
