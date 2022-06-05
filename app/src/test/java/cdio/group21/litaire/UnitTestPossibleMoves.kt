a98b083abb8b9f32f772c5d94230a12b6f360dcb
Zahed <zahedm45@gmail.com>
2022-06-05 18:02:04 +0200

    GameLogic: Added a few more states to the test

--- app/src/test/java/cdio/group21/litaire/UnitTestPossibleMoves.kt
+++ app/src/test/java/cdio/group21/litaire/UnitTestPossibleMoves.kt
@@ -19,84 +19,7 @@ class UnitTestPossibleMoves {
     var foundation: ArrayList<DetectionResult> = ArrayList()
     var tableaus: ArrayList<SortedResult> = ArrayList()
 
-    @Test
-    fun allPossibleMoves() {
 
-
-        // Adding cards to the foundations
-        foundation.add(DetectionResult(RectF(), 100, Card(9, "d")))
-        foundation.add(DetectionResult(RectF(), 100, Card(5, "h")))
-        foundation.add(DetectionResult(RectF(), 100, Card(1, "s")))
-        //foundation.add(DetectionResult(RectF(), 100, Card(4, "c")))
-
-
-        for (i in 0..6) {
-            val k = SortedResult(0f, 0f, ArrayList())
-            tableaus.add(k)
-        }
-
-
-        //  Adding cards to the tableaus
-        tableaus[0].block.add(DetectionResult(RectF(), 100, Card(5, "s")))
-        tableaus[0].block.add(DetectionResult(RectF(), 100, Card(3, "h")))
-        tableaus[0].block.add(DetectionResult(RectF(), 100, Card(2, "c")))
-
-
-        tableaus[1].block.add(DetectionResult(RectF(), 100, Card(4, "h")))
-        tableaus[1].block.add(DetectionResult(RectF(), 100, Card(3, "d")))
-        tableaus[1].block.add(DetectionResult(RectF(), 100, Card(10, "d")))
-
-
-        tableaus[2].block.add(DetectionResult(RectF(), 100, Card(4, "h")))
-        tableaus[2].block.add(DetectionResult(RectF(), 100, Card(3, "c")))
-
-
-        tableaus[3].block.add(DetectionResult(RectF(), 100, Card(2, "d")))
-        tableaus[3].block.add(DetectionResult(RectF(), 100, Card(2, "s")))
-
-
-        tableaus[4].block.add(DetectionResult(RectF(), 100, Card(1, "c")))
-
-
-
-
-
-        // Initializing the expected moves
-        val move1 = Move(true, Card(10, "d"), 1, 0)
-        val move2 = Move(true, Card(2, "s"), 3, 2)
-        val move3 = Move(false, Card(2, "d"), 3, 2)
-        val move4 = Move(true, Card(1, "c"), 4, -1)
-
-
-
-
-
-
-        var result = GameLogic.allPossibleMoves(foundation, tableaus)
-
-        assertEquals(result.size, 4)
-
-        assertEquals(result.contains(move1), true)
-        assertEquals(result.contains(move2), true)
-        assertEquals(result.contains(move3), true)
-        assertEquals(result.contains(move4), true)
-
-
-
-
-        // Initializing an unexpected move
-        val move5 = Move(true, Card(3, "c"), 2, 3)
-        assertEquals(result.contains(move5), false)
-
-
-
-        foundation.add(DetectionResult(RectF(), 100, Card(4, "c")))
-        result = GameLogic.allPossibleMoves(foundation, tableaus)
-
-        assertEquals(result.size, 3)
-
-
-    }
 
     @Test
     fun possibleMovesFromBlockToBlock() {
@@ -240,6 +163,107 @@ class UnitTestPossibleMoves {
         }
 
         assertEquals(returnVal.size, 3)
+
+    }
+
+
+
+
+
+    @Test
+    fun allPossibleMoves() {
+
+
+        // Adding cards to the foundations
+        foundation.add(DetectionResult(RectF(), 100, Card(9, "d")))
+        foundation.add(DetectionResult(RectF(), 100, Card(5, "h")))
+        foundation.add(DetectionResult(RectF(), 100, Card(1, "s")))
+        //foundation.add(DetectionResult(RectF(), 100, Card(4, "c")))
+
+
+        for (i in 0..6) {
+            val k = SortedResult(0f, 0f, ArrayList())
+            tableaus.add(k)
+        }
+
+
+        //  Adding cards to the tableaus
+        tableaus[0].block.add(DetectionResult(RectF(), 100, Card(5, "s")))
+        tableaus[0].block.add(DetectionResult(RectF(), 100, Card(3, "h")))
+        tableaus[0].block.add(DetectionResult(RectF(), 100, Card(2, "c")))
+
+
+        tableaus[1].block.add(DetectionResult(RectF(), 100, Card(4, "h")))
+        tableaus[1].block.add(DetectionResult(RectF(), 100, Card(3, "d")))
+        tableaus[1].block.add(DetectionResult(RectF(), 100, Card(10, "d")))
+
+
+        tableaus[2].block.add(DetectionResult(RectF(), 100, Card(4, "h")))
+        tableaus[2].block.add(DetectionResult(RectF(), 100, Card(3, "c")))
+
+
+        tableaus[3].block.add(DetectionResult(RectF(), 100, Card(2, "d")))
+        tableaus[3].block.add(DetectionResult(RectF(), 100, Card(2, "s")))
+
+
+        tableaus[4].block.add(DetectionResult(RectF(), 100, Card(1, "c")))
+
+
+
+
+
+        // Initializing the expected moves
+        val move1 = Move(true, Card(10, "d"), 1, 0)
+        val move2 = Move(true, Card(2, "s"), 3, 2)
+        val move3 = Move(false, Card(2, "d"), 3, 2)
+        val move4 = Move(true, Card(1, "c"), 4, -1)
+
+
+
+
+
+
+        var result = GameLogic.allPossibleMoves(foundation, tableaus)
+
+        assertEquals(result.size, 4)
+
+        assertEquals(result.contains(move1), true)
+        assertEquals(result.contains(move2), true)
+        assertEquals(result.contains(move3), true)
+        assertEquals(result.contains(move4), true)
+
+
+
+
+        // Initializing an unexpected move
+        val move5 = Move(true, Card(3, "c"), 2, 3)
+        assertEquals(result.contains(move5), false)
+
+
+
+
+        foundation.add(DetectionResult(RectF(), 100, Card(4, "c")))
+        result = GameLogic.allPossibleMoves(foundation, tableaus)
+
+        assertEquals(result.size, 3)
+
+
+
+
+
+
+        // Adding another card which is possible to move
+        tableaus[4].block.add( DetectionResult(RectF(), 10, Card(8, "s")))
+        tableaus[4].block.add( DetectionResult(RectF(), 10, Card(5, "null")))
+
+
+        tableaus[6].block.add(DetectionResult(RectF(), 10, Card(9, "h")))
+
+
+        result = GameLogic.allPossibleMoves(foundation, tableaus)
+        assertEquals(result.size, 4)
+
+
 
     }
 }
