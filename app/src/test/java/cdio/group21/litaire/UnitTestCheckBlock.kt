026643900a6dc208b9ed3e9683f07752e6d6b106
Hildibjørg <s164539@student.dtu.dk>
2022-06-07 16:28:39 +0200

    UnitTest: Tests checkBlock function

---
+++ app/src/test/java/cdio/group21/litaire/UnitTestCheckBlock.kt
@@ -0,0 +1,37 @@
+package cdio.group21.litaire
+
+import cdio.group21.litaire.data.Card
+import cdio.group21.litaire.viewmodels.solver.GameLogic
+import org.junit.Assert
+import org.junit.Test
+
+class UnitTestCheckBlock {
+
+    private var foundation: ArrayList<Card> = ArrayList()
+    private val blocks: ArrayList<ArrayList<Card>> = ArrayList()
+
+    @Test
+    fun checkBlock(){
+
+        for (i in 0..6) {
+            blocks.add(ArrayList())
+        }
+
+
+        blocks[0].add(Card(1, 's'))
+        blocks[0].add(Card(6, 'h'))
+        blocks[0].add(Card(5, 's'))
+
+
+        var returnVal = GameLogic.checkBlock(blocks[0])
+
+        if (returnVal != null) {
+            Assert.assertEquals(returnVal.size, 2)
+        }
+        else
+        {
+            Assert.assertEquals(1+1,3)
+        }
+
+    }
+}
