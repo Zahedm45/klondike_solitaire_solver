3b9250121e1ec15a4680ba873ecec56579c3a748
Hildibjørg <s164539@student.dtu.dk>
2022-06-08 14:47:35 +0200

    Solver: Eval Waste functions made

--- app/src/main/java/cdio/group21/litaire/viewmodels/solver/GameLogic.kt
+++ app/src/main/java/cdio/group21/litaire/viewmodels/solver/GameLogic.kt
@@ -31,7 +31,7 @@ class GameLogic {
 
 
                 if (lastCard.value == (1).toByte() && foundations.size < 4) {
-                    val newMove = Move(true, lastCard,  indexBlock.toByte(), -1)
+                    val newMove = Move(true, lastCard, indexBlock.toByte(), -1)
 
                     possibleMoves.add(newMove)
 
@@ -40,7 +40,7 @@ class GameLogic {
                         val foundation = foundations[k]
                         if (evalBlockToFoundation(foundation, lastCard)) {
 
-                            val newMove = Move(true, lastCard,  indexBlock.toByte(), k.toByte())
+                            val newMove = Move(true, lastCard, indexBlock.toByte(), k.toByte())
                             possibleMoves.add(newMove)
                         }
                     }
@@ -59,14 +59,14 @@ class GameLogic {
         * of possible cards to be moved
         * in a block
         */
-        fun checkBlock(block: ArrayList<Card>):  ArrayList<Card>? {
+        fun checkBlock(block: ArrayList<Card>): ArrayList<Card>? {
             // check if block is empty
-            if(block.isEmpty()) {
+            if (block.isEmpty()) {
                 return null
             }
 
             //should start from the back of the array (first visible card in block)
-            var cur_index = block.size-1
+            var cur_index = block.size - 1
             var tempBlock: ArrayList<Card> = ArrayList()
 
             //add the first visible card, as 1 card should always be moved (unless empty)
@@ -74,15 +74,14 @@ class GameLogic {
             cur_index--
 
             //checks if the rest of the block is in an increasing order and adds them if so
-            while (cur_index >= 0){
+            while (cur_index >= 0) {
                 val deCard = block[cur_index]
                 val seCard = tempBlock.last()
 
-                if(evalBlockToBlock(deCard,seCard)){
+                if (evalBlockToBlock(deCard, seCard)) {
                     tempBlock.add(deCard)
 
-                }
-                else {
+                } else {
                     break
                 }
                 cur_index--
@@ -95,13 +94,12 @@ class GameLogic {
             blocks: ArrayList<ArrayList<Card>>,
             indexBlock: Int,
             possibleMoves: ArrayList<Move>
-        ){
+        ) {
 
 
             val retVal = checkBlock(sourceBlock)
 
-            if(retVal == null)
-            {
+            if (retVal == null) {
                 println("")
                 return
             }
@@ -116,7 +114,12 @@ class GameLogic {
 
                     if (sourceCard.value == (13).toByte()) {
                         if (hasChecked && emptyBlockIndex >= 0) {
-                            val newMove = Move(false, sourceCard, indexBlock.toByte(), emptyBlockIndex.toByte())
+                            val newMove = Move(
+                                false,
+                                sourceCard,
+                                indexBlock.toByte(),
+                                emptyBlockIndex.toByte()
+                            )
                             possibleMoves.add(newMove)
 
                         } else if (!hasChecked) {
@@ -124,7 +127,8 @@ class GameLogic {
 
                             for (iter in blocks.indices) {
                                 if (blocks[iter].isEmpty()) {
-                                    val newMove = Move(false, sourceCard, indexBlock.toByte(), iter.toByte())
+                                    val newMove =
+                                        Move(false, sourceCard, indexBlock.toByte(), iter.toByte())
                                     possibleMoves.add(newMove)
                                     hasChecked = true
                                     emptyBlockIndex = iter
@@ -141,7 +145,7 @@ class GameLogic {
                         break
 
 
-                    }  else {
+                    } else {
 
                         val destCard = blocks[k].last()
                         if (evalBlockToBlock(destCard, sourceCard)) {
@@ -166,7 +170,7 @@ class GameLogic {
             val num2 = foundation.value
 
             if (suit == suit2) {
-                if(num2-num == -1){
+                if (num2 - num == -1) {
                     return true
                 }
             }
@@ -174,7 +178,7 @@ class GameLogic {
         }
 
 
-        fun evalBlockToBlock(destination: Card, source: Card): Boolean{
+        fun evalBlockToBlock(destination: Card, source: Card): Boolean {
 
             val suit = destination.suit
             val num = destination.value
@@ -182,17 +186,15 @@ class GameLogic {
             val suit2 = source.suit
             val num2 = source.value
 
-            if(suit == 's' || suit == 'c'){
-                if (suit2 == 'h' || suit2 == 'd'){
-                    if(num-num2 == 1) {
+            if (suit == 's' || suit == 'c') {
+                if (suit2 == 'h' || suit2 == 'd') {
+                    if (num - num2 == 1) {
                         return true
                     }
                 }
-            }
-
-            else{
-                if (suit2 == 's' || suit2 == 'c'){
-                    if(num-num2 == 1) {
+            } else {
+                if (suit2 == 's' || suit2 == 'c') {
+                    if (num - num2 == 1) {
                         return true
                     }
                 }
@@ -201,5 +203,24 @@ class GameLogic {
             return false
         }
 
+        fun evalWasteToFoundation(waste: Card, destination: Card): Boolean {
+            val foundation = evalBlockToFoundation(destination,waste)
+
+            if(foundation)
+            {
+                return true
+            }
+            return false
+        }
+
+        fun evalWasteToBlock(waste: Card, destination: Card): Boolean {
+            val block = evalBlockToBlock(destination,waste)
+
+            if(block)
+            {
+                return true
+            }
+            return false
+        }
     }
 }
