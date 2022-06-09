9f6883706b05c6ac81e35e27cef75cb87dbd437b
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-06-09 12:46:18 +0200

    Added firebase storage to the api

---
+++ app/src/main/java/cdio/group21/litaire/API/MockImage.kt
@@ -0,0 +1,7 @@
+package cdio.group21.litaire.API
+
+public class MockImage {
+    companion object{
+
+    }
+}
--- app/src/main/java/cdio/group21/litaire/API/RoboflowAPI.kt
+++ app/src/main/java/cdio/group21/litaire/API/RoboflowAPI.kt
@@ -1,64 +1,87 @@
 package cdio.group21.litaire.API
 
+import android.content.Context
 import android.graphics.Bitmap
-import android.os.Build
-import android.util.Base64
 import android.util.Log
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import io.matthewnelson.component.base64.Base64
+import io.matthewnelson.component.base64.encodeBase64
 import okhttp3.MediaType
-import okhttp3.MultipartBody
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.logging.HttpLoggingInterceptor
 import org.json.JSONObject
 import retrofit2.*
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
+import uy.klutter.core.uri.UrlEncoding
 import java.io.ByteArrayOutputStream
+import java.io.File
 
 
 interface RoboflowService {
-    @Headers("Content-Type: application/x-www-form-urlencoded")
-    @Multipart
+    // @Headers("Content-Type: application/x-www-form-urlencoded")
     @POST("1?api_key=UeJ5YANadgDkeozKB2F4&name=image.jpg/")
-    fun getPrediction(@Part imagePart : MultipartBody.Part ): Call<RoboflowResult?>?
+    fun getPrediction(@Body body: String): Call<RoboflowResult?>?
 }
 
 
-class RoboflowAPI{
-    companion object{
+class RoboflowAPI {
+    companion object {
 
+        val storage = Firebase.storage
 
+        private fun toRequestBody(value: String): RequestBody {
+            return value.toRequestBody("text/plain".toMediaTypeOrNull())
+        }
 
-
-        public suspend fun getPrediction(image: Bitmap): RoboflowResult? {
-             var retrofit = Retrofit.Builder()
+        fun getPrediction(context: Context, image: Bitmap): RoboflowResult? {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val httpClient = OkHttpClient.Builder()
+// add your other interceptors …
+// add logging as last interceptor
+// add your other interceptors …
+// add logging as last interceptor
+            httpClient.addInterceptor(logging)
+            var retrofit = Retrofit.Builder()
                 .baseUrl("https://detect.roboflow.com/playing-cards-ow27d/")
                 .addConverterFactory(MoshiConverterFactory.create())
-                .client(OkHttpClient())
+                .client(httpClient.build())
                 .build()
-             var service: RoboflowService = retrofit.create(RoboflowService::class.java)
+            var service: RoboflowService = retrofit.create(RoboflowService::class.java)
 
             val byteArray = ByteArrayOutputStream()
-            image.compress(Bitmap.CompressFormat.JPEG, 70, byteArray)
+            image.compress(Bitmap.CompressFormat.JPEG, 0, byteArray)
             val byteArr = byteArray.toByteArray()
-            val b64 = Base64.encodeToString(byteArr, Base64.URL_SAFE)
+            val base64: Base64 = Base64.UrlSafe(pad = true)
+            var b64 = byteArr.encodeBase64(base64 = base64)
 
-            print(b64)
-            val imagePart = MultipartBody.Part.createFormData(
-                "image",
-                "image.jpg",
-                RequestBody.create(
-                    MediaType.parse("image/jpeg"),
-                    b64
-                )
-            )
 
-            val res = service.getPrediction(imagePart)?.execute()
-            if(res?.errorBody() != null){
+            val path = context.getExternalFilesDir(null)
+            val letDirectory = File(path, "CDIO 21")
+            letDirectory.mkdirs()
+            val file = File(letDirectory, "Image64.txt")
+            b64 = UrlEncoding.encodeQueryNameOrValue(b64)
+            file.writeText(b64)
+
+            val map: MutableMap<String, RequestBody> = mutableMapOf()
+            map.put("image", toRequestBody(b64));
+
+            val request = service.getPrediction(b64)
+            val res = request?.execute()
+            if (res?.errorBody() != null) {
                 val data = res.errorBody()!!.string();
                 val jObjError = JSONObject(data)
-                //Log.e("Roboflow", "Base64 image: ${b64}")
-                //Log.e("Roboflow",  jObjError.getString("error"))
+                Log.e("Roboflow", jObjError.getString("error"))
+                Log.e("Roboflow", res.message())
+                Log.e("Roboflow", res.raw().request.body?.contentType().toString())
+
             }
+            println(res?.body().toString())
             return res?.body()
         }
     }
