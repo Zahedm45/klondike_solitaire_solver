9454012bb6cef8693cf98b95c85ffb79bf3eb268
Zahed <zahedm45@gmail.com>
2022-06-10 00:29:36 +0200

    Solver: Added a card deck, similar to the one we will get to solve

--- app/src/main/java/cdio/group21/litaire/viewmodels/solver/UtilSolver.kt
+++ app/src/main/java/cdio/group21/litaire/viewmodels/solver/UtilSolver.kt
@@ -5,9 +5,13 @@ import cdio.group21.litaire.data.Card
 class UtilSolver {
     companion object {
 
+        val cardDeck : ArrayList<Card> = ArrayList()
+
+
         fun simulateRandomCards(
             foundation: ArrayList<Card>,
-            blocks: ArrayList<ArrayList<Card>>
+            blocks: ArrayList<ArrayList<Card>>,
+            waste: Card
         ) {
 
 /*            foundation.add(Card(9, "d"))
@@ -19,13 +23,45 @@ class UtilSolver {
             val values: Array<Byte> = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
 
 
+
             for (i in 0..6) {
-                //val k = SortedResult(0f, 0f, ArrayList())
                 blocks.add(ArrayList())
             }
 
 
             for (suit in suits) {
+
+                for (value in values) {
+                    val card = Card(value, suit)
+                    cardDeck.add(card)
+                }
+            }
+
+            cardDeck.shuffle()
+            cardDeck.shuffle()
+            cardDeck.shuffle()
+
+
+
+            for (i in 0..6) {
+
+                var k = i +1
+                while (k > 0) {
+                    blocks[i].add(cardDeck.last())
+                    cardDeck.removeLast()
+                    k--
+                }
+
+            }
+
+
+            waste.value = cardDeck.last().value
+            waste.suit = cardDeck.last().suit
+
+
+
+
+/*            for (suit in suits) {
 
                 for (value in values) {
 
@@ -35,12 +71,12 @@ class UtilSolver {
                     blocks[randomInt].add(card)
 
                 }
-            }
+            }*/
 
 
-            blocks.forEach {
+/*            blocks.forEach {
                 it.shuffle()
-            }
+            }*/
 
         }
 
