f7124bb1ead687b393308b70c047dc21286f6154
Hildibjørg <s164539@student.dtu.dk>
2022-06-09 13:20:44 +0200

    Move: Changed name for a variable

--- app/src/main/java/cdio/group21/litaire/data/Move.kt
+++ app/src/main/java/cdio/group21/litaire/data/Move.kt
@@ -3,6 +3,6 @@ package cdio.group21.litaire.data
 data class Move(
     val isMoveToFoundation: Boolean,
     val card: Card,
-    val indexOfBlock : Byte, // source, 8 = waste
+    val indexOfSourceBlock : Byte, // source, 8 = waste
     val indexOfDestination : Byte
 )
