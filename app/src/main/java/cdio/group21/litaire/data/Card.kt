6455e34178ecb4bd679c91279126e7e4bb3677df
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-06-17 13:50:08 +0200

    Solitaire: Add method Card::equals
    This makes it a lot easier to compare cards.

--- app/src/main/java/cdio/group21/litaire/data/Card.kt
+++ app/src/main/java/cdio/group21/litaire/data/Card.kt
@@ -178,5 +178,23 @@ data class Card(var suit: Suit, var rank: Rank) {
         return Card(suit.copy(), rank.copy())
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Card
+
+        if (suit != other.suit) return false
+        if (rank != other.rank) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = suit.hashCode()
+        result = 31 * result + rank.hashCode()
+        return result
+    }
+
 
 }
